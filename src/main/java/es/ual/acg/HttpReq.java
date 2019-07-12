13056d1c4365808ae321681b3b86afbe175962c7
Manel <manelme@outlook.com>
2019-07-12 13:56:01 +0200

    adapted to Java 11

--- src/main/java/es/ual/acg/HttpReq.java
+++ src/main/java/es/ual/acg/HttpReq.java
@@ -1,15 +1,15 @@
 package es.ual.acg;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.URL;
-import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
-import java.util.StringJoiner;
+import java.util.concurrent.CompletableFuture;
+import java.util.ArrayList;
+import java.util.List;
+
+import java.net.http.*;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.URI;
 
 public class HttpReq {
 
@@ -19,57 +19,51 @@ public class HttpReq {
 
     }
 
-    public String sendPostRequest(String address, HashMap<String, String> headers, HashMap<String, String> payload) {
-        String response = "sin procesar";
-        try {
-            URL myurl = new URL(address);
+    public CompletableFuture sendPostRequestPlain(String address, HashMap<String, String> headers, String payload) {
+            var client = HttpClient.newHttpClient();
 
-            con = (HttpURLConnection) myurl.openConnection();
-            con.setDoOutput(true);
-            con.setRequestMethod("POST");
-            headers.forEach((k, v) -> con.setRequestProperty(k, v));
-            StringJoiner sj = new StringJoiner("&");
-            payload.forEach((k, v) -> {
-                try {
-                    sj.add(URLEncoder.encode(k, "UTF-8") + "=" + URLEncoder.encode(v, "UTF-8"));
-                } catch (UnsupportedEncodingException e) {
-                    e.printStackTrace();
-                }
-            });
+            List<String> headersArrayList =  new ArrayList<>();
+            headers.forEach((k,v)-> {headersArrayList.add(k); headersArrayList.add(v);});
+            String[] headersArray = new String[headersArrayList.size()];
 
-            byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8);
-            int length = out.length;
-
-            con.setFixedLengthStreamingMode(length);
-            con.connect();
-            try(OutputStream os = con.getOutputStream()) {
-                os.write(out);
-            }
             
-            int responceCode = con.getResponseCode();
+            var request = HttpRequest.newBuilder()
+                            .POST(BodyPublishers.ofString(payload))
+                            .uri(URI.create(address))
+                            .headers(headersArrayList.toArray(headersArray))
+                            .build();
+                            
+        
+            return client.sendAsync(request, BodyHandlers.ofString())
+                            .thenApply(HttpResponse::body)
+                            .exceptionally(e -> "Error: " + e.getMessage());
 
-            if (responceCode == HttpURLConnection.HTTP_OK)
-            {
-                String line;
-                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
-                while ((line = br.readLine()) != null)
-                {
-                    response = "";
-                    response += line;
-                }
-            }
-            else 
-            {
-                response = "";
-            }
-            return response;
 
-        }catch(Exception e) {
-            e.printStackTrace();
-            return e.getMessage();
-        }finally {
-            con.disconnect();
-        }
+            /*
+             * URL myurl = new URL(address);
+             * 
+             * con = (HttpURLConnection) myurl.openConnection(); con.setDoOutput(true);
+             * con.setRequestMethod("POST"); headers.forEach((k, v) ->
+             * con.setRequestProperty(k, v)); StringJoiner sj = new StringJoiner("&");
+             * payload.forEach((k, v) -> { try { sj.add(URLEncoder.encode(k, "UTF-8") + "="
+             * + URLEncoder.encode(v, "UTF-8")); } catch (UnsupportedEncodingException e) {
+             * e.printStackTrace(); } });
+             * 
+             * byte[] out = sj.toString().getBytes(StandardCharsets.UTF_8); int length =
+             * out.length;
+             * 
+             * con.setFixedLengthStreamingMode(length); con.connect(); try(OutputStream os =
+             * con.getOutputStream()) { os.write(out); }
+             * 
+             * int responceCode = con.getResponseCode();
+             * 
+             * if (responceCode == HttpURLConnection.HTTP_OK) { String line; BufferedReader
+             * br = new BufferedReader(new InputStreamReader(con.getInputStream())); while
+             * ((line = br.readLine()) != null) { response = ""; response += line; } } else
+             * { response = ""; } return response;
+             * 
+             * }catch(Exception e) { e.printStackTrace(); return e.getMessage(); }finally {
+             * con.disconnect(); }
+             */
     }
-
 }
--- src/test/java/es/ual/acg/HttpReqTest.java
+++ src/test/java/es/ual/acg/HttpReqTest.java
@@ -12,11 +12,17 @@ public class HttpReqTest{
     public void TestPostReq() {
         String address = "https://postman-echo.com/post";
         HashMap<String, String> headers = new HashMap<>();
-        headers.put("content-type", "application/json");
-        HashMap<String, String> payload = new HashMap<>();
-        payload.put("foo1","bar1");
-        payload.put("foo2","bar2");
+        headers.put("content-type", "text/plain");
+        headers.put("Authorization", "no me jodas");
+        String payload = "foo";
         HttpReq tester = new HttpReq();
-        assertNotEquals(tester.sendPostRequest(address, headers, payload),"");
+        try{
+            var response = tester.sendPostRequestPlain(address, headers, payload).get();
+            System.out.println(response);
+            assertNotEquals(response,"");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
     }
 }
