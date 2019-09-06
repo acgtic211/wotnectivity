527b6191a09f5f99a7e4adf634119069745a4c0c
Manel <manelme@outlook.com>
2019-09-06 09:40:54 +0200

    http+test done

--- src/main/java/es/ual/acg/HttpReq.java
+++ src/main/java/es/ual/acg/HttpReq.java
@@ -1,6 +1,5 @@
 package es.ual.acg;
 
-import java.net.HttpURLConnection;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.ArrayList;
@@ -13,14 +12,14 @@ import java.net.URI;
 
 public class HttpReq {
 
-    private HttpURLConnection con;
+    private HttpClient client;
 
     public HttpReq() {
-
+        this.client = HttpClient.newHttpClient();
     }
 
     public CompletableFuture sendPostRequest(String address, HashMap<String, String> headers, String payload) {
-            var client = HttpClient.newHttpClient();
+            
 
             List<String> headersArrayList =  new ArrayList<>();
             headers.forEach((k,v)-> {headersArrayList.add(k); headersArrayList.add(v);});
@@ -41,7 +40,6 @@ public class HttpReq {
     }
 
     public CompletableFuture sendGetRequest(String address, HashMap<String, String> headers) {
-        var client = HttpClient.newHttpClient();
 
         List<String> headersArrayList =  new ArrayList<>();
         headers.forEach((k,v)-> {headersArrayList.add(k); headersArrayList.add(v);});
@@ -60,4 +58,44 @@ public class HttpReq {
                         .exceptionally(e -> "Error: " + e.getMessage());
 
     }
+    
+    public CompletableFuture sendPutRequest(String address, HashMap<String, String> headers, String payload) {
+
+
+        List<String> headersArrayList =  new ArrayList<>();
+        headers.forEach((k,v)-> {headersArrayList.add(k); headersArrayList.add(v);});
+        String[] headersArray = new String[headersArrayList.size()];
+
+        var request = HttpRequest.newBuilder()
+                        .PUT(BodyPublishers.ofString(payload))
+                        .uri(URI.create(address))
+                        .headers(headersArrayList.toArray(headersArray))
+                        .build();
+                        
+    
+        return client.sendAsync(request, BodyHandlers.ofString())
+                        .thenApply(HttpResponse::body)
+                        .exceptionally(e -> "Error: " + e.getMessage());
+
+    }
+
+    public CompletableFuture sendDeleteRequest(String address, HashMap<String, String> headers) {
+
+        List<String> headersArrayList =  new ArrayList<>();
+        headers.forEach((k,v)-> {headersArrayList.add(k); headersArrayList.add(v);});
+        String[] headersArray = new String[headersArrayList.size()];
+
+        var request = HttpRequest.newBuilder()
+                        .DELETE()
+                        .uri(URI.create(address))
+                        .headers(headersArrayList.toArray(headersArray))
+                        .build();
+                        
+    
+        return client.sendAsync(request, BodyHandlers.ofString())
+                        .thenApply(HttpResponse::body)
+                        .exceptionally(e -> "Error: " + e.getMessage());
+
+    }
+
 }
--- src/test/java/es/ual/acg/HttpReqTest.java
+++ src/test/java/es/ual/acg/HttpReqTest.java
@@ -54,4 +54,51 @@ public class HttpReqTest{
         }
         
     }
+
+    @Test
+    public void TestPutReq() {
+        String address = "https://postman-echo.com/put?q=adios";
+        HashMap<String, String> headers = new HashMap<>();
+        HashMap<String, String> headers2 = new HashMap<>();
+        headers.put("content-type", "text/plain");
+        headers.put("Authorization", "no me jodas");
+        headers2.put("content-type", "application/json");
+        headers2.put("Authorization", "no me jodas");
+        String payload = "{\"body\":\"vaya tela\"}";
+
+        HttpReq tester = new HttpReq();
+        try{
+            var response = tester.sendPutRequest(address, headers, payload).get();
+            var response2 = tester.sendPutRequest(address, headers2, payload).get();
+            System.out.println(response);
+            System.out.println(response2);
+            assertNotEquals(response2,"");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
+
+    @Test
+    public void TestDeleteReq() {
+        String address = "https://postman-echo.com/delete";
+        HashMap<String, String> headers = new HashMap<>();
+        HashMap<String, String> headers2 = new HashMap<>();
+        headers.put("content-type", "text/plain");
+        headers.put("Authorization", "no me jodas");
+        headers2.put("content-type", "application/json");
+        headers2.put("Authorization", "no me jodas");
+
+        HttpReq tester = new HttpReq();
+        try{
+            var response = tester.sendDeleteRequest(address, headers).get();
+            var response2 = tester.sendDeleteRequest(address, headers2).get();
+            System.out.println(response);
+            System.out.println(response2);
+            assertNotEquals(response2,"");
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        
+    }
 }
